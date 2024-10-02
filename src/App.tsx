0a8d2c4d3f1331a0d7230933a941cdec2ba06a99
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-10-02 23:05:14 +0545

    using shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import Grid from "./component/grid";
 import { twMerge } from "tailwind-merge";
-import ModeToggle from "./component/mode-toggle";
+import ThemeSwitch from "@/theme/switch";
 
 function App() {
-  const [dark, setDark] = useState(true);
   return (
-    <div
-      className={twMerge(
-        "size-full p-2 overflow-auto h-screen",
-        dark ? "bg-black text-white dark" : "bg-white text-black"
-      )}>
+    <div className={twMerge("size-full p-2 overflow-auto h-screen")}>
       <div className="w-full p-3">
-        <ModeToggle dark={dark} setDark={(mode: boolean) => setDark(mode)} />
+        <ThemeSwitch />
       </div>
       <Grid />
     </div>
